3b4c3eb5510cc6be54f9710ded409a23dbe7e18c
Sara Lotfi <lotfi.sara@gmail.com>
2021-02-18 13:53:24 +0100

    add ingredient seeds to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Ingredient.destroy_all
+
+require 'open-uri'
+require 'json'
+
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+json_string  = open(url).read
+json_hash = JSON.parse(json_string)
+
+
+Ingredient.create(name: "Light rum")
+Ingredient.create(name: "Applejack")
+Ingredient.create(name: "Lime juice")
+Ingredient.create(name: "Cherries")
+Ingredient.create(name: "Apricot brandy")
+Ingredient.create(name: "Orange bitters")
+Ingredient.create(name: "Soda water")
+Ingredient.create(name: "Kahlua")
+Ingredient.create(name: "Tea")
+Ingredient.create(name: "Champagne")
+
