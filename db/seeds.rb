f5f9a5315c944854fc60290dc1812f275d6abdac
Sara Lotfi <lotfi.sara@gmail.com>
2021-02-19 16:06:52 +0100

    added cloudinary

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,23 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Ingredient.destroy_all
+# Ingredient.destroy_all if Rails.env.development?
 
-require 'open-uri'
-require 'json'
+# require 'open-uri'
+# require 'json'
 
-url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
-json_string  = open(url).read
-json = JSON.parse(json_string)
+# url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+# json_string  = open(url).read
+# json = JSON.parse(json_string)
 
-json["drinks"].each do |hash|
-  value = hash["strIngredient1"]
-  Ingredient.create!(name: value)
-end
+# json["drinks"].each do |hash|
+#   value = hash["strIngredient1"]
+#   Ingredient.create!(name: value)
+# end
+
+Ingredient.create!(name: "Mint leaves")
+Ingredient.create!(name: "Cointreau")
+Ingredient.create!(name: "Olive")
+Ingredient.create!(name: "Lemon peel")
+Ingredient.create!(name: "Orange peel")
 
