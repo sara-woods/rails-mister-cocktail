fa86a9799a11955935be2cdd4803701a64ae25f9
Sara Lotfi <lotfi.sara@gmail.com>
2021-02-20 19:33:31 +0100

    remove double  validation

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -2,9 +2,7 @@ class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
 
-  validates :description, presence: true
   validates :cocktail, presence: true
   validates :description, presence: true
   validates :ingredient, uniqueness: { scope: :cocktail, message: "can only be added once" }
-  
 end
